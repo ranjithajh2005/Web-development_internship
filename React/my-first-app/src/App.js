2a3389eb8236a8e03d5d970981227ce5202d0a11
ranjithajh2005 <ranjithajh2026@gmail.com>
2026-06-27 01:14:02 +0530

    react+vite

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,20 @@
+import react from "react";
+import Button from "./LoginButton";
+
+function App(){
+
+
+  return(
+    <div style={{color:'blue',border:'1px solid black',width:'400px',margin:'auto',textAlign:'center',backgroundColor:'lightblue'}}>
+      <h2>Hello, React!</h2>
+      <p>This is my first React class</p>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+
+    </div>
+  )
+}
+export default App
